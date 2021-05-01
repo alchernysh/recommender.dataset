3defb350160d113ce4efd4c5e878f3e416373e59
alchernysh <aleksei.0.chernysh>
2021-05-01 22:43:25 +0300

    fix: correct filtration

--- src/data_filter.py
+++ src/data_filter.py
@@ -7,7 +7,7 @@ def filter_data():
     with Path('data/desc.json').open() as f:
         data = json.load(f)
     print(len(data))
-    filtered_data = list(filter(lambda x: x['length'] >= 2000 and len(x['tags']>=8), data))
+    filtered_data = list(filter(lambda x: x['length'] >= 2000 and len(x['tags']) >= 8, data))
     print(len(filtered_data))
     with Path('data/desc_filtered.json').open('w') as f:
         json.dump(filtered_data, f)
